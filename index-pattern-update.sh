a4573164acbd5b7d71894522be4bd4235871e31c
Arezki Ouhenia <arezki@octodet.com>
2021-12-29 20:20:25 +0000

    Add new file

---
+++ index-pattern-update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+for rule in $(pwd)/*.toml; do
+                     
+       rulename=${rule##*/}
+       extension="${rulename##*.}"
+       filename="${rulename%.*}"
+        
+       grep -iq $1 $rule   
+       if [ $? == 1 ]
+       then
+                lineindex=`grep -n "^index =" $rule | awk -F: '{print $1}'`
+                linethreatindex=`grep -n "^threat_index =" $rule | awk -F: '{print $1}'`
+
+                sed -i'.original' "${lineindex}s/\*/&$1*/g" $rule
+                
+                if diff "$rule" "$rule.original";then
+                    sed -i '/^index =/,/\]/{/^index =/n;/\]/!{s/\*/&cipher*/g}}' $rule 
+                fi
+
+                if [ ! -z "$linethreatindex" ]
+                then
+                    sed -i "${linethreatindex}s/\*/&$1*/g" $rule
+                fi
+                rm -f "$rule.original"
+        fi
+done
